6ac36593aab5c79c380a5c3822d5930941c31eea
Yamnyr <quentinwaroquet@gmail.comm>
2025-02-14 10:21:03 +0100

    details

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ from movie_details import show_movie_details
 # Configuration de la page
 st.set_page_config(page_title="Liste des films", page_icon="🎬", layout="wide")
 
-st.title("🎬 Bienvenue sur l'application TMDb")
-st.write("Utilisez cette application pour importer et visualiser des films depuis TMDb.")
+# st.title("🎬 Bienvenue sur l'application TMDb")
+# st.write("Utilisez cette application pour importer et visualiser des films depuis TMDb.")
 
 if "selected_movie" in st.session_state and st.session_state.selected_movie:
     show_movie_details()
